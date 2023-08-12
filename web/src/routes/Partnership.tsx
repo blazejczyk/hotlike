af35ac5c77d68ef3f669fc03131451db305c9dbc
Eryk Blazejczyk <eryk.blazejczyk@gmail.com>
2023-08-12 12:47:46 +0200

    Partnership image

--- web/src/routes/Partnership.tsx
+++ web/src/routes/Partnership.tsx
@@ -1,4 +1,5 @@
-import { useCallback, FormEvent } from 'react';
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
 
 import Article, { Importance } from '../components/Article';
 import Paragraph from '../components/Paragraph';
@@ -7,17 +8,15 @@ import useTitle from '../hooks/useTitle';
 function Partnership() {
   useTitle('Partnership');
 
-  const handleSend = useCallback((evt: FormEvent<HTMLFormElement>) => {
-    evt.preventDefault();
-    console.log('handle send');
-  }, []);
-
   return (
-    <Article title="Would you like to be our business partner?">
+    <Article title="Would you like to be our business partner?" contentCss={styles}>
       <Paragraph>
         If you represent a company or a place helping to meet people together (e.g.: coffee shop, restaurant, club, pub or
         actually any kind of entertainment), <Importance>HotLike</Importance> may be an interesting option for you to
         gain some new clients very spontaneously.
+        <div className="partnership-container">
+          <img src="/images/partnership.png" className="partnership" />
+        </div>
       </Paragraph>
       <Paragraph>
         People using our app usually meet in such places so it's obvious they can be a good target for your business.
@@ -32,5 +31,16 @@ function Partnership() {
     </Article>
   );
 }
+
+const styles = css`
+  .partnership-container {
+    text-align: center;
+    padding-top: 10px;
+  }
+  .partnership {
+    width: 100%;
+    max-width: 400px;
+  }
+`;
 
 export default Partnership;
